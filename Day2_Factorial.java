401154061ec0d4debe1e3b63d37646c0efb9f482
Bhagyashree S A <184914610+Bhagya-SA@users.noreply.github.com>
2025-11-07 19:06:15 +0530

    Add files via upload

---
+++ Day2_Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Day2_Factorial {
+
+    static int fact (int n) {
+        int res = 1;
+        for (int i = 1; i <= n; i++){
+            res*=i;
+        }
+    
+        return res;
+    }
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+
+        sc.close();
+
+    }
+    
+}
